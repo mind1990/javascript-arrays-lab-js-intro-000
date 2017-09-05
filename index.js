eb89384679fcbce99dbc4736ac3d400048200185
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-05 01:09:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,6 +31,6 @@ function prependKitten(name) {
 }
 
 function removeLastKitten() {
-  var newkittens = kittens.slice(2, kittens.length - 1)
+  var newkittens = kittens.slice(2, kittens.length)
   return newkittens
 }
