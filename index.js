246ef7e039a88bcb4db076bb5ca6a5a70582fa2f
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-05 00:54:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-const app = "I don't do much."
+
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+  return kittens
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+  return kittens
+}
+
+function destructivelyRemoveLastKitten(name) {
+  kittens.pop(name)
+  return kittens
+}
+
+function destructivelyRemoveFirstKitten(name) {
+  kittens.shift(name)
+  return kittens
+}
+
+function appendKitten(name) {
+  var newkittens = [...kittens, name]
+  return newkittens
+}
+
+function prependKitten(name) {
+  var newkittens = [name, ...kittens]
+  return newkittens
+}
+
+function removeLastKitten() {
+  kittens.slice(-1)
+  return kittens
+}
